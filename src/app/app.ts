232fba406e4ece835575a9028f283bf246e0c209
Ben <ben@_._>
2025-12-01 19:41:26 -0600

    ...

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, inject, TemplateRef } from '@angular/core';
-import { getDefaultConfirmationDialogConfig, ModalConfirmation } from './modal-confirmation';
 import { Dialog } from '@angular/cdk/dialog';
+import { DIALOG_DATA, DialogConfig, DialogRef } from '@angular/cdk/dialog';
+import { ChangeDetectionStrategy } from '@angular/core';
 
-export interface NeighborOut {
+ interface NeighborOut {
   id: string;
   firstName: string;
   lastName: string;
@@ -12,7 +13,7 @@ export interface NeighborOut {
   properties: PropertyOut[];
 }
 
-export interface PropertyOut {
+ interface PropertyOut {
   lotNumber: string;
   streetNumber: number | null;
 }
@@ -48,3 +49,108 @@ export class AppComponent {
     this.#dialog.open(ModalConfirmation, dialogConfig);
   }
 }
+
+ interface IConfirmationModal<TSubSet> {
+  title: string;
+  maximizable: boolean;
+  hideCloseX: boolean;
+  body: TemplateRef<void>;
+  hasActionButton: boolean;
+  actionButtonText?: string;
+  actionButtonSeverity?: Severity;
+  dismissButtonText: string;
+  dismissButtonSeverity?: Severity;
+  subset?: TSubSet;
+}
+
+ type Severity =
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'info'
+  | 'warn'
+  | 'help'
+  | 'danger'
+  | 'plain';
+
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <div style="border: 1px solid red">
+      <div>
+        Modal content
+      </div>
+      @if(data.hasActionButton){
+        <button (click)="handleActionButtonClick()">
+        {{ data.actionButtonText }}
+        </button>
+      }
+      <button (click)="handleDismissButtonClick()">
+        {{ data.dismissButtonText }}
+      </button>
+    </div>
+  `
+})
+class ModalConfirmation<TSubSet> {
+  readonly #dialogRef = inject(DialogRef<TSubSet>);
+  readonly data: IConfirmationModal<TSubSet> = inject(DIALOG_DATA);
+
+  handleDismissButtonClick(): void {
+    this.#dialogRef.close();
+  }
+
+  handleActionButtonClick(): void {
+    this.#dialogRef.close(this.data.subset);
+  }
+}
+
+
+
+ const getDefaultConfirmationDialogConfig = <TDataSubset>({
+  disableClose = true,
+  maximizable = false,
+  hideCloseX = false,
+  width,
+  title,
+  body,
+  hasActionButton,
+  actionButtonText,
+  actionButtonSeverity,
+  dismissButtonText,
+  dismissButtonSeverity,
+  dataSubset
+}: {
+  disableClose?: boolean;
+  maximizable?: boolean;
+  hideCloseX?: boolean;
+  width?: string;
+  title: string;
+  body: TemplateRef<void>;
+  hasActionButton: boolean;
+  actionButtonText?: string;
+  actionButtonSeverity?: Severity;
+  dismissButtonText: string;
+  dismissButtonSeverity?: Severity;
+  dataSubset?: TDataSubset;
+}): DialogConfig<
+  IConfirmationModal<TDataSubset>,
+  DialogRef<TDataSubset, ModalConfirmation<TDataSubset>>
+> => {
+  return {
+    disableClose,
+    width,
+    data: {
+      title,
+      maximizable,
+      hideCloseX,
+      body,
+      hasActionButton,
+      actionButtonText,
+      actionButtonSeverity,
+      dismissButtonText,
+      dismissButtonSeverity,
+      subset: dataSubset
+    }
+  };
+};
